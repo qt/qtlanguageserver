ee69e589e233c75d3be731115d423271d89bdaa6
Marc Mutz <marc.mutz@qt.io>
2023-07-13 12:42:50 +0200

    QIOPipePrivate: fix Clang's -Wfinal-dtor-non-final-class
    Says Clang 15:
    
        qiopipe.cpp:39:23: warning: class with destructor marked 'final' cannot be inherited from [-Wfinal-dtor-non-final-class]
            ~QIOPipePrivate() final;
                          ^
        qiopipe.cpp:34:7: note: mark 'QIOPipePrivate' as 'final' to silence this warning
        class QIOPipePrivate : public QObjectPrivate
              ^
    
    Apply the suggested fix. Cf. also
    qtdeclarative/d432e5621a0a163ea5609229ef94f83235de4050.
    
    Change-Id: If083ea0c4aba57119030c1de031c825999bff923
    Reviewed-by: Fabian Kosmale <fabian.kosmale@qt.io>
    (cherry picked from commit b429e880e38764eaff841ff338601ac34fafaa03)
    Reviewed-by: Qt Cherry-pick Bot <cherrypick_bot@qt-project.org>

--- tests/auto/qlanguageserver/qiopipe.cpp
+++ tests/auto/qlanguageserver/qiopipe.cpp
@@ -31,12 +31,12 @@ private:
     QPointer<QPipeEndPoint> m_remoteEndPoint;
 };
 
-class QIOPipePrivate : public QObjectPrivate
+class QIOPipePrivate final : public QObjectPrivate
 {
     Q_DECLARE_PUBLIC(QIOPipe)
 public:
     QIOPipePrivate();
-    ~QIOPipePrivate() final;
+    ~QIOPipePrivate() override;
 
     std::unique_ptr<QPipeEndPoint> end1;
     std::unique_ptr<QPipeEndPoint> end2;
