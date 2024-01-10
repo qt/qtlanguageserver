3b482c7286f4a42d59c89e76a0186b821778ccc1
Joerg Bornemann <joerg.bornemann@qt.io>
2024-01-10 14:39:13 +0100

    Enable CMake autotests
    Task-number: QTBUG-84884
    Change-Id: I0ea101dd39096ec0b0a135e3de3a9d3ac9b98404
    Reviewed-by: Alexandru Croitor <alexandru.croitor@qt.io>

--- tests/auto/cmake/test_qtjsonrpc_module/main.cpp
+++ tests/auto/cmake/test_qtjsonrpc_module/main.cpp
@@ -1,7 +1,7 @@
 // Copyright (C) 2019 The Qt Company Ltd.
 // SPDX-License-Identifier: LicenseRef-Qt-Commercial OR GPL-3.0-only WITH Qt-GPL-exception-1.0
 
-#include <QtJsonRpc/qjsonrpcprotocol.h>
+#include <private/qjsonrpcprotocol_p.h>
 
 int main(int argc, char **argv)
 {
